a03eabc34750aae8f04da6415f4b6f1b06e2f620
6kerokerotm9 <luckystripes60@gmail.com>
2019-11-20 00:40:39 -0800

    Add files via upload

--- barrel_shifter.v
+++ barrel_shifter.v
@@ -21,7 +21,12 @@ input [31:0] D;
 input [31:0] S;
 input LnR;
 
-// TBD
+wire [31:0] temp_output;
+wire or_result;
+
+BARREL_SHIFTER32 shift(temp_output, D, S[5:0], LnR);
+or unary(or_result, S[31:5]);
+MUX32_2x1 final(Y, temp_output, 32'b0, or_result);
 
 endmodule
 
@@ -34,8 +39,11 @@ input [31:0] D;
 input [4:0] S;
 input LnR;
 
-// TBD
+wire [31:0] shift_left, shift_right;
 
+SHIFT32_L left(shift_left,D,S);
+SHIFT32_R right(shift_right,D,S);
+MUX32_2x1 choice(Y, shift_right, shift_left, LnR);
 endmodule
 
 // Right shifter
@@ -46,7 +54,82 @@ output [31:0] Y;
 input [31:0] D;
 input [4:0] S;
 
-// TBD
+wire [31:0] mux1_result, mux2_result, mux3_result, mux4_result; //results of each row of bit shifters
+
+genvar i;
+generate
+for(i = 31; i >= 0; i = i - 1) //first row of bit shifters
+begin : left_shift_loop_1
+  if(i == 31)
+  begin
+    MUX1_2x1 mux(mux1_result[i], D[i], 0, S[0]); //load the first instance with 0 
+  end
+  else
+  begin
+    MUX1_2x1 mux(mux1_result[i], D[i], D[i+1], S[0]); 
+  end
+end
+endgenerate
+
+genvar j;
+generate
+for(j = 31; j >= 0; j = j - 1) //second row of bit shifters
+begin : left_shift_loop_2
+  if(j >= 30)
+  begin
+    MUX1_2x1 mux(mux2_result[j], mux1_result[j], 0, S[1]); //load the first instance with 0 
+  end
+  else
+  begin
+    MUX1_2x1 mux(mux2_result[j], mux1_result[j], mux1_result[j+2], S[1]); 
+  end
+end
+endgenerate
+
+genvar k;
+generate
+for(k = 31; k >= 0; k = k - 1) //third row of bit shifters
+begin : left_shift_loop_3
+  if(k >= 28)
+  begin
+    MUX1_2x1 mux(mux3_result[k], mux2_result[k], 0, S[2]); //load the first instance with 0 
+  end
+  else
+  begin
+    MUX1_2x1 mux(mux3_result[k], mux2_result[k], mux2_result[k+4], S[2]); 
+  end
+end
+endgenerate
+
+genvar l;
+generate
+for(l = 31; l >= 0; l = l - 1) //fourth row of bit shifters
+begin : left_shift_loop_4
+  if(l >= 24)
+  begin
+    MUX1_2x1 mux(mux4_result[l], mux3_result[l], 0, S[3]); //load the first instance with 0 
+  end
+  else
+  begin
+    MUX1_2x1 mux(mux4_result[l], mux3_result[l], mux3_result[l+8], S[3]); 
+  end
+end
+endgenerate
+
+genvar m;
+generate
+for(m = 31; m >= 0; m = m - 1) //fourth row of bit shifters
+begin : left_shift_loop_5
+  if(m >= 16)
+  begin
+    MUX1_2x1 mux(Y[m], mux4_result[m], 0, S[4]); //load the first instance with 0 
+  end
+  else
+  begin
+    MUX1_2x1 mux(Y[m], mux4_result[m], mux4_result[m+16], S[4]); 
+  end
+end
+endgenerate
 
 endmodule
 
@@ -94,13 +177,13 @@ genvar k;
 generate
 for(k = 0; k < 32; k = k + 1) //third row of bit shifters
 begin : left_shift_loop_3
-  if(k <= 2)
+  if(k <= 4)
   begin
     MUX1_2x1 mux(mux3_result[k], mux2_result[k], 0, S[2]); //load the first instance with 0 
   end
   else
   begin
-    MUX1_2x1 mux(mux3_result[k], mux2_result[k], mux2_result[k-3], S[2]); 
+    MUX1_2x1 mux(mux3_result[k], mux2_result[k], mux2_result[k-4], S[2]); 
   end
 end
 endgenerate
@@ -109,13 +192,13 @@ genvar l;
 generate
 for(l = 0; l < 32; l = l + 1) //fourth row of bit shifters
 begin : left_shift_loop_4
-  if(l <= 3)
+  if(l <= 8)
   begin
     MUX1_2x1 mux(mux4_result[l], mux3_result[l], 0, S[3]); //load the first instance with 0 
   end
   else
   begin
-    MUX1_2x1 mux(mux4_result[l], mux3_result[l], mux3_result[l-4], S[3]); 
+    MUX1_2x1 mux(mux4_result[l], mux3_result[l], mux3_result[l-8], S[3]); 
   end
 end
 endgenerate
@@ -124,13 +207,13 @@ genvar m;
 generate
 for(m = 0; m < 32; m = m + 1) //fourth row of bit shifters
 begin : left_shift_loop_5
-  if(m <= 4)
+  if(m <= 16)
   begin
     MUX1_2x1 mux(Y[m], mux4_result[m], 0, S[4]); //load the first instance with 0 
   end
   else
   begin
-    MUX1_2x1 mux(Y[m], mux4_result[m], mux4_result[m-5], S[3]); 
+    MUX1_2x1 mux(Y[m], mux4_result[m], mux4_result[m-16], S[4]); 
   end
 end
 endgenerate
--- barrel_shifter_tb.v
+++ barrel_shifter_tb.v
@@ -4,12 +4,24 @@ module barrel_shifter_tb;
 wire [31:0] Y;
 reg [31:0] D;
 reg [5:0] S;
+reg LnR;
 
-SHIFT32_L left(Y,D,S);
+//SHIFT32_L left(Y,D,S);
+//SHIFT32_R right(Y,D,S);
+//BARREL_SHIFTER32 shift(Y,D,S, LnR);
+SHIFT32 shift(Y,D,S, LnR);
 
 initial
 begin
-  #5 D = 32; S = 2;
-  #5 D = 2; S = 4;
+  #5 D = 32; S = 2; LnR = 0;
+  #5 D = 31; S = 0; LnR = 0;
+  #5 D = 32; S = 2; LnR = 1;
+  #5 D = 15; S = 10; LnR = 1;
+  #5 D = 16; S = 4; LnR = 0; 
+  #5 D = 16; S = 3; LnR = 1;
+  #5 D = 16; S = 32; LnR = 1;
+  #5 D = 12; S = 64; LnR = 0;
+  /*#5 D = 2; S = 4;
+  #5 D = 15; S = 16;*/
 end
 endmodule; 
